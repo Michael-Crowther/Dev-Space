fa23526879d215677ce80f1ec47abee37471047c
Michael-Crowther <md.crowther@yahoo.com>
2024-10-15 08:17:30 -0600

    Testing fix for login issue on prod

--- src/components/providers/UserProvider.tsx
+++ src/components/providers/UserProvider.tsx
@@ -2,7 +2,7 @@
 
 import { api } from "@/app/api/trpc/util";
 import { UserProfile } from "@/server/shared/routerTypes";
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 import { createContext, ReactNode, useContext } from "react";
 import { ClipLoader } from "react-spinners";
 
@@ -13,7 +13,7 @@ type UserContext = {
 export const UserContext = createContext<UserContext | undefined>(undefined);
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const router = useRouter();
+  //const router = useRouter();
 
   const { data: user, isLoading } = api.base.user.loggedIn.useQuery();
 
@@ -21,9 +21,9 @@ export function UserProvider({ children }: { children: ReactNode }) {
     return <ClipLoader />;
   }
 
-  if (!user) {
-    router.push("/login");
-  }
+  //   if (!user) {
+  //     router.push("/login");
+  //   }
 
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
